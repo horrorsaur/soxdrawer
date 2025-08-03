5d8b191b888122ec70f9491f41d4b6a7fafcd994
garyhost@nixos <horrorsaur@gmail.com>
2025-08-03 11:19:11 +0000

    oopsie, add the actual vite config changes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -16,5 +15,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/index.js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   },
 }) 
